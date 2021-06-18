9a040a7f15b27bbe1c5ffc371d59239220ce60e3
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/uniform_random_.js
+++
@@ -1,5 +0,0 @@
-export function uniform_random_() {
-    return Math.random();
-}
-export { uniform_random_ as _random__uniform };
-//# sourceMappingURL=src/uniform_random_.js.map
--- src/uniform_random_int_.js
+++
@@ -1,7 +0,0 @@
-import { distribution_random_int_ } from './distribution_random_int_';
-import { uniform_random_ } from './uniform_random_';
-export function uniform_random_int_(min = 0, max = 1) {
-    return distribution_random_int_(min, max, uniform_random_);
-}
-export { uniform_random_int_ as _int__random__uniform };
-//# sourceMappingURL=src/uniform_random_int_.js.map
